9c32a37a6e64b9b5acc8cba126be873c77efe448
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2022-12-08 18:01:47 +0100

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ function App() {
   return (
     <Wrapper>
       <InputWrapper>
-        <Hint>Use <strong>Tab</strong>, <strong>ArrowUp</strong> or <strong>ArrowDownkeys</strong><br />to navigate results. Hit <strong>Enter</strong> to see result.</Hint>
+        <Hint>Use <strong>Tab</strong>, <strong>ArrowUp</strong> or <strong>ArrowDown</strong> keys<br />to navigate results. Hit <strong>Enter</strong> to see result.</Hint>
         <ClearIcon onClick={handleClearValue}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
